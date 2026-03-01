af258d13e9756e7a5e808fd92fb0a280c27074ef
David Killion <davidkillion12@gmail.com>
2026-03-01 10:38:08 -0500

    add site builder link

---
+++ src/pages/MasterIndex.tsx
@@ -0,0 +1,8 @@
+export default function MasterIndex() {
+  return (
+    <div style={{ padding: "2rem", color: "white" }}>
+      <h1>Master Index</h1>
+      <p>This is the React-driven Master Index page.</p>
+    </div>
+  );
+}
