d26ab1d816fa0952462a36467e6fe603ea154a4c
David Killion <davidkillion12@gmail.com>
2026-03-01 11:08:16 -0500

    Add MasterIndex route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import MasterIndex from "./pages/MasterIndex";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -19,6 +19,8 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+           
+            <Route path="/master-index" element={<MasterIndex />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
