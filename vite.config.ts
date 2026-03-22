f2c1fb3107cfef9b12d5c829ca79b45429575f63
David Killion <davidkillion12@gmail.com>
2026-03-21 21:45:22 -0400

    mmmmm????

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/site_builder/',
+export default defineConfig({
+  base: "/site_builder/",
   server: {
-    host: "::",
+    host: true,
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  plugins: [react()],
+});
